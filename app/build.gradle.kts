bcf5b305eab02891f01e59a3a425937e089d3a53
Nicolas Aleixo <75820713+aleixo-dev@users.noreply.github.com>
2025-04-28 19:46:13 -0300

    [bump version] 1.1.1 (#6)
    * chore: bump version to 1.1.1
    
    * refactor: remove unnecessary modifier props (#7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nicolas.composepriceformatter"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.1"
 
         proguardFiles("consumer-rules.pro")
 
